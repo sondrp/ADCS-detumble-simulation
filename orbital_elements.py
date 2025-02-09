dc2f1341aca985caf768061f22edb98b2ef36218
sondrp <sondrp@stud.ntnu.no>
2025-02-09 12:52:39 +0100

    package orbital elements into class

--- orbital_elements.py
+++ orbital_elements.py
@@ -1,10 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-M = 5.9722e24
-G = 6.6743e-11
-
-gravitational_parameter = G * M  # the greek letter mu in literature
 
 # Solve Kepler's equation for E
 #   M = E - e sin E
@@ -25,34 +21,20 @@ def newton_rhapson(M, e, tol=10e-12):
     return E
 
 
-class RotationMatrix:
+class OrbitalElements:
+    
+    # G * M
+    mu = 5.9722e24 * 6.6743e-11 
 
-    # We only need the transpose of the first two rows of the 3-1-3 Euler rotation matrix
-    def __init__(self, raan=None, aop=None, i=None):
-        self.A = None
-        self._create_matrix(raan, aop, i)
-
-    def multiply(self, x, y):
-        if self.A is None:
-            return None
-
-        vector = np.array([x, y])
-        return self.A @ vector
-
-    # Make sure the stored matrix is correct
-    def prepare(self, raan, aop, i):
-        if raan == self.raan and aop == self.aop and i == self.i:
-            return
-
-        self._create_matrix(raan, aop, i)
-
-    def _create_matrix(self, raan, aop, i):
-        self.raan = raan
-        self.aop = aop
-        self.i = i
-
-        if raan is None or aop is None or i is None:
-            return
+    def __init__(self, a, e, m0, i, raan, aop):
+        """
+        a   : [float] semimajor axis
+        e   : [float] eccentricity
+        m0  : [float] initial mean anomaly
+        i   : [float] inclination
+        raan: [float] right ascension of the ascending node (uppercase Omega)
+        aop : [float] argument of perigee                   (lowercase omega)
+        """
 
         A11 = np.cos(raan) * np.cos(aop) - np.sin(raan) * np.sin(aop) * np.cos(i)
         A12 = np.sin(raan) * np.cos(aop) + np.cos(raan) * np.sin(aop) * np.cos(i)
@@ -62,6 +44,10 @@ class RotationMatrix:
         A22 = -np.sin(raan) * np.sin(aop) + np.cos(raan) * np.cos(aop) * np.cos(i)
         A23 = np.cos(raan) * np.sin(i)
 
+        self.a = a
+        self.e = e
+        self.m0 = m0
+
         self.A = np.array(
             [
                 [A11, A21],
@@ -70,50 +56,39 @@ class RotationMatrix:
             ]
         )
 
+    def state(self, t):
+        """
+        t: time since passage of perigee
+        returns: array([x1, x2, x3, v1, v2, v3]) # position and velocity
+        """
+        a, e, m0 = self.a, self.e, self.m0
+        mu, A = self.mu, self.A
 
-rotation_matrix = RotationMatrix()
+        n = np.sqrt(mu / a**3)
 
+        m = m0 + n * t
 
-def orbital_elements_to_position_and_velocity(a, e, m0, i, raan, aop, t):
-    """
-    a   : [float] semimajor axis
-    e   : [float] eccentricity
-    m0  : [float] initial mean anomaly
-    i   : [float] inclination
-    raan: [float] right ascension of the ascending node (uppercase Omega)
-    aop : [float] argument of perigee                   (lowercase omega)
-    t   : [float] time since perigee passage
+        E = newton_rhapson(m, e)
 
-    returns: position and velocity vectors at time t in orbit
-    """
+        r = a * (1 - e * np.cos(E))
+        x = a * (np.cos(E) - e)
+        y = a * np.sqrt(1 - e * e) * np.sin(E)
 
-    n = np.sqrt(gravitational_parameter / a**3)
+        xdot = -(n * a * a / r) * np.sin(E)
+        ydot = (n * a * a / r) * np.sqrt(1 - e * e) * np.cos(E)
 
-    m = m0 + n * t
+        pos = A @ np.array([x, y])
+        vel = A @ np.array([xdot, ydot])
 
-    E = newton_rhapson(m, e)
-
-    r = a * (1 - e * np.cos(E))
-    x = a * (np.cos(E) - e)
-    y = a * np.sqrt(1 - e * e) * np.sin(E)
-
-    xdot = -(n * a * a / r) * np.sin(E)
-    ydot = (n * a * a / r) * np.sqrt(1 - e * e) * np.cos(E)
-
-    rotation_matrix.prepare(raan, aop, i)
-
-    pos = rotation_matrix.multiply(x, y)
-    vel = rotation_matrix.multiply(xdot, ydot)
-
-    return pos, vel
+        return pos, vel
 
 
 # Example:
 
 a = 1.0  # AU
-e = 0   # the ISS has very close to 0 inclination
+e = 0  # the ISS has very close to 0 eccentricity
 
-i = 56*np.pi / 180  
+i = 56 * np.pi / 180  # inclination of ISS
 raan = 0.0
 aop = 0.0
 m = 0.0
@@ -124,11 +99,13 @@ tout = np.linspace(0, 2, steps)
 xout = np.zeros((steps, 3))
 vout = np.zeros((steps, 3))
 
+orbital_elements = OrbitalElements(a, e, m, i, raan, aop)
+
 for index, t in enumerate(tout):
-  x, v = orbital_elements_to_position_and_velocity(a, e, m, i, raan, aop, t)
-  xout[index] = x
-  vout[index] = v
- 
+    x, v = orbital_elements.state(t)
+    xout[index] = x
+    vout[index] = v
+
 fig = plt.figure()
 ax1 = fig.add_subplot(111, projection="3d")
 
@@ -136,10 +113,12 @@ x = xout[:, 0]
 y = xout[:, 1]
 z = xout[:, 2]
 
-ax1.scatter(x, y, z, c='b', marker='o')
+ax1.scatter(x, y, z, c="b", marker="o")
 
 # Set equal aspect ratio
-max_range = np.array([x.max() - x.min(), y.max() - y.min(), z.max() - z.min()]).max() / 2.0
+max_range = (
+    np.array([x.max() - x.min(), y.max() - y.min(), z.max() - z.min()]).max() / 2.0
+)
 mid_x = (x.max() + x.min()) / 2.0
 mid_y = (y.max() + y.min()) / 2.0
 mid_z = (z.max() + z.min()) / 2.0
@@ -152,10 +131,10 @@ ax1.set_xlabel("X Axis")
 ax1.set_ylabel("Y Axis")
 ax1.set_zlabel("Z Axis")
 
-ax1.set_xlabel('X Axis')
-ax1.set_ylabel('Y Axis')
-ax1.set_zlabel('Z Axis')
-ax1.set_title('3D Surface Plot')
+ax1.set_xlabel("X Axis")
+ax1.set_ylabel("Y Axis")
+ax1.set_zlabel("Z Axis")
+ax1.set_title("3D Surface Plot")
 
 plt.tight_layout()
 plt.show()
@@ -166,4 +145,3 @@ plt.plot(tout, vout[:, 1])
 plt.plot(tout, vout[:, 2])
 plt.plot(tout, vnorm)
 plt.show()
-
